7ab7869dd44bc97628c50bc79e963d072634cc93
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2023-01-13 17:14:56 +0530

    Categories Screen

---
+++ lib/category_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  // const CategoryItem({super.key});
+  final String title;
+  final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        color: color,
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
